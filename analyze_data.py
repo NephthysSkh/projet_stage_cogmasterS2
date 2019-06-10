882cab988104541291d2ac19200f90f324b04cf2
NephthysSkh <45490524+NephthysSkh@users.noreply.github.com>
2019-06-10 22:13:05 +0200

    Update

--- analyze_data.py
+++ analyze_data.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from distances import get_midpoints
 
@@ -201,28 +202,28 @@ def get_distance_moments(feats_1, feats_2, n_repr=0):
 if __name__ == '__main__' :
     parser = argparse.ArgumentParser(
         description='Compute distance matrix between features extracted from corpus 1 and corpus 2')
-    #parser.add_argument('wav_folder_path_1', metavar='path_1', type=str,
-                        #help='path to wav folder containing the data of corpus 1')
-    #parser.add_argument('wav_folder_path_2', metavar='path_2', type=str,
-                        #help='path to wav folder containing the data of corpus 2')
-    #parser.add_argument('path_alignment_file_1', metavar='alignment_file', type=str,
-                        #help='path to the alignment file 1')
-    #parser.add_argument('path_alignment_file_2', metavar='alignment_file', type=str,
-                        #help='path to the alignment file 2')
-    #parser.add_argument('save_path_norm_data_1', metavar='save_norm_1', type=str,
-                        #help='path to csv file where normalized feature chart of corpus 1 is saved')
-    #parser.add_argument('save_path_norm_data_2', metavar='save_norm_2', type=str,
-                        #help='path to csv file where normalized feature chart of corpus 2 is saved')
-    #parser.add_argument('data_corpus_1', metavar='data_corpus_1', type=str,
-                        #help='csv file of normalized features of corpus 1 phones')
-    #parser.add_argument('data_corpus_2', metavar='data_corpus_2', type=str,
-                        #help='csv file of normalized features of corpus 2 phones')
+    # parser.add_argument('wav_folder_path_1', metavar='path_1', type=str,
+    #                     help='path to wav folder containing the data of corpus 1')
+    # parser.add_argument('wav_folder_path_2', metavar='path_2', type=str,
+    #                     help='path to wav folder containing the data of corpus 2')
+    # parser.add_argument('path_alignment_file_1', metavar='alignment_file', type=str,
+    #                     help='path to the alignment file 1')
+    # parser.add_argument('path_alignment_file_2', metavar='alignment_file', type=str,
+    #                     help='path to the alignment file 2')
+    # parser.add_argument('save_path_norm_data_1', metavar='save_norm_1', type=str,
+    #                     help='path to csv file where normalized feature chart of corpus 1 is saved')
+    # parser.add_argument('save_path_norm_data_2', metavar='save_norm_2', type=str,
+    #                     help='path to csv file where normalized feature chart of corpus 2 is saved')
+    # parser.add_argument('data_corpus_1', metavar='data_corpus_1', type=str,
+    #                     help='csv file of normalized features of corpus 1 phones')
+    # parser.add_argument('data_corpus_2', metavar='data_corpus_2', type=str,
+    #                     help='csv file of normalized features of corpus 2 phones')
     parser.add_argument('selected_data_corpus_1', metavar='selected_data_corpus_1', type=str,
                         help='save path for randomly selected lines of the data_corpus_1 csv files')
     parser.add_argument('selected_data_corpus_2', metavar='selected_data_corpus_2', type=str,
                         help='save path for randomly selected lines of the data_corpus_2 csv files')
-    #parser.add_argument('nb_of_rows', metavar='nb_of_rows', type=int,
-                        #help='number of selected rows')
+    # parser.add_argument('nb_of_rows', metavar='nb_of_rows', type=int,
+    #                     help='number of selected rows')
     parser.add_argument('n_repr', default=0, metavar='n_repr', type=int,
                         help='optional maximum number of representatives of a phoneme to use')
 
@@ -235,14 +236,14 @@ if __name__ == '__main__' :
     #     args.save_path_norm_data_1, args.save_path_norm_data_2
     # )
 
-    #select_data(
-    #    args.data_corpus_1, args.data_corpus_2, 
-    #    args.selected_data_corpus_1, args.selected_data_corpus_2, 
-    #    args.nb_of_rows)
+    # select_data(
+    #     args.data_corpus_1, args.data_corpus_2, 
+    #     args.selected_data_corpus_1, args.selected_data_corpus_2, 
+    #     args.nb_of_rows)
 
     distances_matrix, stdev_distances_matrix = compute_distances_matrix(
-        args.selected_data_corpus_1, args.selected_data_corpus_2, args.n_repr
-    )
+         args.selected_data_corpus_1, args.selected_data_corpus_2, args.n_repr
+     )
 
     distances_matrix.to_csv('distances_matrix.csv')
     stdev_distances_matrix.to_csv('distances_matrix_stdev.csv')
@@ -253,7 +254,7 @@ if __name__ == '__main__' :
     plt.xlabel("phones corpus 2 (English)")
     plt.ylabel("phones corpus 1 (French)")
     plt.savefig('/scratch2/elannelongue/distance_matrix.pdf')
-    #plt.show()
+    plt.show()
 
     #means_per_speaker = calculate_mean_per_speaker('toy_data', 'toy_data/toy_data_alignment.txt', 'features_data_1.csv', 'norm_data_1.csv', 'mean_data_1.csv')
     #arguments : toy_data_1 toy_data_2 alignment/toy_data_alignment_1.txt alignment/toy_data_alignment_2.txt norm_data_1.csv norm_data_2.csv
